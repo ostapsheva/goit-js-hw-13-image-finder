28be3054c0393dec5d649724685d50be4aba2763
Ostap Shevchenko <ostapshev@gmail.com>
2020-08-20 18:57:12 +0300

    add task 13

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,9 +1,12 @@
 import refs from './refs.js';
 
 function scroll() {
+  console.log('refs.gallery.clientHeight', refs.gallery.clientHeight);
+  console.log('refs.form.clientHeight', refs.form.clientHeight);
+
   setTimeout(() => {
     window.scrollTo({
-      top: refs.gallery.clientHeight + refs.form.clientHeight,
+      top: refs.gallery.clientHeight,
       behavior: 'smooth',
     });
   }, 1000);
